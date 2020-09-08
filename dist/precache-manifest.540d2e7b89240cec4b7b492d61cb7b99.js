69d8942c9398ac9ce1b0982e09638438435b8c7a
CesarxArteaga <ctmces94@gmail.com>
2020-09-08 13:40:27 -0500

    dist

---
+++ dist/precache-manifest.540d2e7b89240cec4b7b492d61cb7b99.js
@@ -0,0 +1,566 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf91856c54d6bad832b3",
+    "url": "/css/about.594bdd28.css"
+  },
+  {
+    "revision": "c390df43b54994280451",
+    "url": "/css/app.844a6d71.css"
+  },
+  {
+    "revision": "66697b0c7dc5ebf1db2f",
+    "url": "/css/chunk-vendors.14427ded.css"
+  },
+  {
+    "revision": "fc56d64946c63472e50799672360c1f9",
+    "url": "/img/ACABADO.fc56d649.png"
+  },
+  {
+    "revision": "b615624bddfbe8166f573a6a8a2de555",
+    "url": "/img/ACOMPAÑARE.b615624b.png"
+  },
+  {
+    "revision": "9b984e6ec1281d560f08a9c7a3def379",
+    "url": "/img/AMIGA.9b984e6e.png"
+  },
+  {
+    "revision": "f6bc6bbf4e86c994f345bcb1d0f9d643",
+    "url": "/img/AYUDA.f6bc6bbf.png"
+  },
+  {
+    "revision": "1786f3099042f5f65d5f86587e6e358c",
+    "url": "/img/BONITA.1786f309.png"
+  },
+  {
+    "revision": "034671a2982867e937954b46cc47fee9",
+    "url": "/img/CONSIGUE.034671a2.png"
+  },
+  {
+    "revision": "c70746f3e39a0975e999c27ad501a9dc",
+    "url": "/img/CONSTRUIR.c70746f3.png"
+  },
+  {
+    "revision": "b20a61dd23df8bac9240ffde23c3f6ac",
+    "url": "/img/CONVERSABAN.b20a61dd.png"
+  },
+  {
+    "revision": "1cf44b4a22797e34fcf1f02b6a0df605",
+    "url": "/img/CUANDO.1cf44b4a.png"
+  },
+  {
+    "revision": "5e74d93cbcce09029e2f12942a8e5632",
+    "url": "/img/CUENTO.5e74d93c.png"
+  },
+  {
+    "revision": "ebe763ed4016933987e507f7bbb25bed",
+    "url": "/img/CUERDA.ebe763ed.png"
+  },
+  {
+    "revision": "7a9782804a48363d219c2e21ac909035",
+    "url": "/img/DA.7a978280.png"
+  },
+  {
+    "revision": "8816a2b0dde5fc1ab1fe65ee5a746b1f",
+    "url": "/img/DIJERON.8816a2b0.png"
+  },
+  {
+    "revision": "4e16170df08ad635c7880df067e41489",
+    "url": "/img/DOS_DEDO.4e16170d.png"
+  },
+  {
+    "revision": "8ec206bc0b11aa69b90a57054aeee153",
+    "url": "/img/ELLA.8ec206bc.png"
+  },
+  {
+    "revision": "717aa683333b95376064b6d47b2d0f54",
+    "url": "/img/ELLA_MAMA.717aa683.png"
+  },
+  {
+    "revision": "cebe687d3213ee7d9a17b216aca43fca",
+    "url": "/img/ELLOS.cebe687d.png"
+  },
+  {
+    "revision": "638612ddf6f16c4ff1bc761bb8e43a2a",
+    "url": "/img/ENCANTA.638612dd.png"
+  },
+  {
+    "revision": "677b306d2432f98206b308e1b48c7e79",
+    "url": "/img/ENVIA.677b306d.png"
+  },
+  {
+    "revision": "0edcdc86f987c7cd86b75691cf53a036",
+    "url": "/img/ESCONDIDO.0edcdc86.png"
+  },
+  {
+    "revision": "00b58ba0cefd566135fdf6dc804ed944",
+    "url": "/img/ESCUCHARLO.00b58ba0.png"
+  },
+  {
+    "revision": "5df6e6b23983a399e75578263ac3d128",
+    "url": "/img/ESTABA.5df6e6b2.png"
+  },
+  {
+    "revision": "560f7b1cd4de9bce987c3c238536d182",
+    "url": "/img/EXITO.560f7b1c.png"
+  },
+  {
+    "revision": "4258c5d584990a0639db1673542aa245",
+    "url": "/img/EXPLICA.4258c5d5.png"
+  },
+  {
+    "revision": "bbf6873a1aee3daaec266505e217cdee",
+    "url": "/img/FASCINA.bbf6873a.png"
+  },
+  {
+    "revision": "72a7cba06b2e8a9bf7f742c0c4b3548d",
+    "url": "/img/FAVORITOS.72a7cba0.png"
+  },
+  {
+    "revision": "ac23bdac54390b16a70d38b8c5a303d5",
+    "url": "/img/GABY-SELE.ac23bdac.jpg"
+  },
+  {
+    "revision": "9139135dfa4f79d36fab2abef3c993ef",
+    "url": "/img/GABY.9139135d.png"
+  },
+  {
+    "revision": "74bbc93ccf5540b0c44ec164a9543e79",
+    "url": "/img/HABLAR_GABY.74bbc93c.png"
+  },
+  {
+    "revision": "9bb8c6f69e14ee14e1ca49e7ea97e05d",
+    "url": "/img/HABLAR_KIKE.9bb8c6f6.png"
+  },
+  {
+    "revision": "0bcccf2a8bad98ed72afab8e82b2b1bf",
+    "url": "/img/HISTORIA-SELE.0bcccf2a.jpg"
+  },
+  {
+    "revision": "2700d7dea7e57f07cf21e24dfbac6258",
+    "url": "/img/HISTORIA.2700d7de.png"
+  },
+  {
+    "revision": "a7f33da9ba100310b1ea8e26d5009342",
+    "url": "/img/HOLA.a7f33da9.png"
+  },
+  {
+    "revision": "6d545ff871ba6757484bfb1052f311c3",
+    "url": "/img/HURRA.6d545ff8.png"
+  },
+  {
+    "revision": "0c571c609191917940d72f5b96df7dac",
+    "url": "/img/IDEA.0c571c60.png"
+  },
+  {
+    "revision": "674f554293a7f755c86c77b4b006ceed",
+    "url": "/img/INVENTO.674f5542.png"
+  },
+  {
+    "revision": "6e0e6c203dce4044516588f2fe3fb62a",
+    "url": "/img/JUGAR.6e0e6c20.png"
+  },
+  {
+    "revision": "39fb2471b4c2c83dbea34bcae9059052",
+    "url": "/img/KIKE-SELE.39fb2471.jpg"
+  },
+  {
+    "revision": "9bf644e6bfee4718d272b568d9d58d12",
+    "url": "/img/KIKE.9bf644e6.png"
+  },
+  {
+    "revision": "05cc4d4ee5fb92649d6fb8f3a5b6dd56",
+    "url": "/img/LIMPIA.05cc4d4e.png"
+  },
+  {
+    "revision": "1d8d1a0c7c3a1a269121882a6484386a",
+    "url": "/img/LLAMO.1d8d1a0c.png"
+  },
+  {
+    "revision": "0b6696574cfff2110af6974e6350c147",
+    "url": "/img/LLEGADO.0b669657.png"
+  },
+  {
+    "revision": "6f0eca8b061ebc50b0f4d0f4f0e15af0",
+    "url": "/img/LOGRADO.6f0eca8b.png"
+  },
+  {
+    "revision": "0b6c472590a61ab168903dddcb919247",
+    "url": "/img/LOLY-SELE.0b6c4725.jpg"
+  },
+  {
+    "revision": "03cf33881b7c8859e5626f8ba815a0ea",
+    "url": "/img/LOLY.03cf3388.png"
+  },
+  {
+    "revision": "62d49fc05e627b72e5c34b510e99a7c9",
+    "url": "/img/MAMAGABY-SELE.62d49fc0.jpg"
+  },
+  {
+    "revision": "94546c8260a28645b90caa2cdc55110d",
+    "url": "/img/MAMAKIKE-SELE.94546c82.jpg"
+  },
+  {
+    "revision": "b003d676277622b183b07589c1356b3a",
+    "url": "/img/MAMA_GABY.b003d676.png"
+  },
+  {
+    "revision": "953ee559bafb5995c7c1f3c63a66ea11",
+    "url": "/img/MARAVILLOSA.953ee559.png"
+  },
+  {
+    "revision": "5c7d74d399f3d9074c015c80cc985322",
+    "url": "/img/MIDI.5c7d74d3.png"
+  },
+  {
+    "revision": "36121ca600f69d179610698feddd4e6f",
+    "url": "/img/NO.36121ca6.png"
+  },
+  {
+    "revision": "91edf549a541fb850895a4317631c233",
+    "url": "/img/OCURRIO.91edf549.png"
+  },
+  {
+    "revision": "b24d0f10207a36322691af3ab1815c96",
+    "url": "/img/PADRES.b24d0f10.png"
+  },
+  {
+    "revision": "956d3868b9dfdfda515d2765208349ac",
+    "url": "/img/PAPAKIKE-SELE.956d3868.jpg"
+  },
+  {
+    "revision": "19cc5803441616029ddf2bf093e44c72",
+    "url": "/img/PARQUE-SELE.19cc5803.jpg"
+  },
+  {
+    "revision": "b71a66d114f924a899e95b1b82d054eb",
+    "url": "/img/PARQUE.b71a66d1.png"
+  },
+  {
+    "revision": "4a553ab00dcdb1e7f3128363dd0a202e",
+    "url": "/img/PINTAR.4a553ab0.png"
+  },
+  {
+    "revision": "ddac43c624537c3a4b29f99f91c9cca4",
+    "url": "/img/PODER.ddac43c6.png"
+  },
+  {
+    "revision": "352ffcf1152f891c25ccab43004bc669",
+    "url": "/img/PODIA.352ffcf1.png"
+  },
+  {
+    "revision": "a2625ee2ec247570ec7de6e7b55c74ad",
+    "url": "/img/PODRA.a2625ee2.png"
+  },
+  {
+    "revision": "fcb3e66793416f4e4afaaab2193feefb",
+    "url": "/img/PODRAN.fcb3e667.png"
+  },
+  {
+    "revision": "beeaee307bee5d7abff1042ba5794f06",
+    "url": "/img/PODRIA.beeaee30.png"
+  },
+  {
+    "revision": "91c6c2306ed004f31bb1463ef03d69cd",
+    "url": "/img/PREOCUPADOS.91c6c230.png"
+  },
+  {
+    "revision": "6c0854687a50133fe6f92d585be1553d",
+    "url": "/img/PROBARLO.6c085468.png"
+  },
+  {
+    "revision": "ebdd0ca8de0ef64ea5590b38afe4d6d4",
+    "url": "/img/PUDO.ebdd0ca8.png"
+  },
+  {
+    "revision": "9413dc0929056db04f7acd463f752ce5",
+    "url": "/img/PUEDE.9413dc09.png"
+  },
+  {
+    "revision": "6ee6c37847c734d494b146b8c8570cf7",
+    "url": "/img/SABEN.6ee6c378.png"
+  },
+  {
+    "revision": "0923341a7dc49aaccbda665387340033",
+    "url": "/img/SI_GABY.0923341a.png"
+  },
+  {
+    "revision": "a6fc61d958928c12502c5c9c5f1580fb",
+    "url": "/img/SUS_KIKE.a6fc61d9.png"
+  },
+  {
+    "revision": "223725045714e6c2a10ec37a7bc587e9",
+    "url": "/img/SU_KIKE.22372504.png"
+  },
+  {
+    "revision": "eb360ff90fcd7f7897a77f99c7ad0d84",
+    "url": "/img/TE.eb360ff9.png"
+  },
+  {
+    "revision": "ff35f18b2e53940b32e9269ba3729fb6",
+    "url": "/img/TERMINAR.ff35f18b.png"
+  },
+  {
+    "revision": "272fe455e2167e7bcfae0a20ae583911",
+    "url": "/img/TODAS.272fe455.png"
+  },
+  {
+    "revision": "1c29c58c54ba4581a913c970708b7ad1",
+    "url": "/img/TODO.1c29c58c.png"
+  },
+  {
+    "revision": "0ff14074a10edbcb789d2169b7bc395b",
+    "url": "/img/TRISTE.0ff14074.png"
+  },
+  {
+    "revision": "5276f1a18d250979c846eb07883a94ae",
+    "url": "/img/VASOFONO-SELE.5276f1a1.jpg"
+  },
+  {
+    "revision": "2c442d2a95d2ee413eb912ca4c580de8",
+    "url": "/img/VASOFONO.2c442d2a.png"
+  },
+  {
+    "revision": "5f18e6dbb40474d3d2ab6e8ac870e920",
+    "url": "/img/VECINA.5f18e6db.png"
+  },
+  {
+    "revision": "0fe71969359a9c77e00a54a7c983484d",
+    "url": "/img/VENCIDO.0fe71969.png"
+  },
+  {
+    "revision": "bf5ecd81569e3233c313225e98d0523d",
+    "url": "/img/VIDEOJUEGOS.bf5ecd81.png"
+  },
+  {
+    "revision": "5d06aac15556860024dd5e26f121bb93",
+    "url": "/img/VIRUS_MALVADO.5d06aac1.png"
+  },
+  {
+    "revision": "423d196b45556ccf4ced0aef2e0dd3a9",
+    "url": "/img/audio_button.423d196b.png"
+  },
+  {
+    "revision": "dbf8657b831117acabbb23ec287578f3",
+    "url": "/img/close_button.dbf8657b.png"
+  },
+  {
+    "revision": "378eeebc2a28a673fa8febcabf445580",
+    "url": "/img/cover.378eeebc.jpg"
+  },
+  {
+    "revision": "ff5d7b1de9f1464ce30256707ad7dd5d",
+    "url": "/img/creditos_button.ff5d7b1d.png"
+  },
+  {
+    "revision": "d7cd3621d46ffa8e5d30c079467cfcc9",
+    "url": "/img/espol.d7cd3621.png"
+  },
+  {
+    "revision": "61a84aec3fea7b7ac691afd0df4fa96c",
+    "url": "/img/fadcom.61a84aec.png"
+  },
+  {
+    "revision": "9ba9e20ac7d2423bac08af161b1605e3",
+    "url": "/img/home_button.9ba9e20a.png"
+  },
+  {
+    "revision": "51eb3c3a9ace68957098c22dd565b462",
+    "url": "/img/horizontal.51eb3c3a.png"
+  },
+  {
+    "revision": "c16ee01ec85ede07641a6f811d61acc2",
+    "url": "/img/logomidi.c16ee01e.png"
+  },
+  {
+    "revision": "da2abc5a39112d4faa77a0c513d2e586",
+    "url": "/img/next.da2abc5a.png"
+  },
+  {
+    "revision": "32bbb202b9fc85613d64921cb2ceb16a",
+    "url": "/img/opening.32bbb202.jpg"
+  },
+  {
+    "revision": "985667b4148d4f0bc8fedbcac5db9d3b",
+    "url": "/img/pasta.985667b4.png"
+  },
+  {
+    "revision": "a8313bb43dfa2d241d289d82f2719841",
+    "url": "/img/prev.a8313bb4.png"
+  },
+  {
+    "revision": "b588fa5678e174487aca924117f611d4",
+    "url": "/img/scene_1.b588fa56.jpg"
+  },
+  {
+    "revision": "7203444bce4d3532460d1271f55fe862",
+    "url": "/img/scene_10.7203444b.jpg"
+  },
+  {
+    "revision": "880cfad862885327ef5f7f36c27fc545",
+    "url": "/img/scene_11.880cfad8.jpg"
+  },
+  {
+    "revision": "b2c090e8de4a91d3e298822e2dc725f1",
+    "url": "/img/scene_12.b2c090e8.jpg"
+  },
+  {
+    "revision": "16fbe36bdc569817d851413fe1ec967a",
+    "url": "/img/scene_13.16fbe36b.jpg"
+  },
+  {
+    "revision": "598510e13a1438fe914591c3cc4a09f2",
+    "url": "/img/scene_14.598510e1.jpg"
+  },
+  {
+    "revision": "4e6e49c56c7911c21b202c80127a961c",
+    "url": "/img/scene_15.4e6e49c5.jpg"
+  },
+  {
+    "revision": "0cc380248b65820f0caef0f2c5c0c58a",
+    "url": "/img/scene_16.0cc38024.jpg"
+  },
+  {
+    "revision": "0c076bedce5b36beef05a2724fff8a11",
+    "url": "/img/scene_17.0c076bed.jpg"
+  },
+  {
+    "revision": "49bee766987fe53f45200b3a7db143cc",
+    "url": "/img/scene_18.49bee766.jpg"
+  },
+  {
+    "revision": "ba15648498925948854ee0fcc6a26148",
+    "url": "/img/scene_2.ba156484.jpg"
+  },
+  {
+    "revision": "c34f63ed8104a5529654f1608003b5ab",
+    "url": "/img/scene_3.c34f63ed.jpg"
+  },
+  {
+    "revision": "4fd30b973b560fc421eb9f8a485665a8",
+    "url": "/img/scene_4.4fd30b97.jpg"
+  },
+  {
+    "revision": "fba1fb2a2c1a09960f0e907098014f43",
+    "url": "/img/scene_5.fba1fb2a.jpg"
+  },
+  {
+    "revision": "8f4b84327b0e83c5789debcd96ed9cfe",
+    "url": "/img/scene_6.8f4b8432.jpg"
+  },
+  {
+    "revision": "28ec70d61f961bf10128a2ac856fceaf",
+    "url": "/img/scene_7.28ec70d6.jpg"
+  },
+  {
+    "revision": "542a4067d80b5cbf45616c5b3cf160bf",
+    "url": "/img/scene_8.542a4067.jpg"
+  },
+  {
+    "revision": "89aea0d8a04cb4d56a7a5a6453f636aa",
+    "url": "/img/scene_9.89aea0d8.jpg"
+  },
+  {
+    "revision": "7e4d1746ba74cceb7d6ad9c5f02d1bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf91856c54d6bad832b3",
+    "url": "/js/about.56282b28.js"
+  },
+  {
+    "revision": "c390df43b54994280451",
+    "url": "/js/app.7a0f770a.js"
+  },
+  {
+    "revision": "66697b0c7dc5ebf1db2f",
+    "url": "/js/chunk-vendors.0d06ec4e.js"
+  },
+  {
+    "revision": "6edfbfe97c055380b0a9f36257615f91",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "3cf966df69f2fbed0b607c7c44170925",
+    "url": "/media/end.3cf966df.mp3"
+  },
+  {
+    "revision": "9b06242d5aa49593b3fea013871dddd0",
+    "url": "/media/opening.9b06242d.mp3"
+  },
+  {
+    "revision": "c79705be4c71e09deaf62506d566058d",
+    "url": "/media/scene_1.c79705be.mp3"
+  },
+  {
+    "revision": "e79cfae21555eb413458c5b6944c6d69",
+    "url": "/media/scene_10.e79cfae2.mp3"
+  },
+  {
+    "revision": "ae1a25c4dac1ba817f0c86d17d3f29c8",
+    "url": "/media/scene_11.ae1a25c4.mp3"
+  },
+  {
+    "revision": "3974b6558b6fe0838eeb1ebe67a0fcca",
+    "url": "/media/scene_12.3974b655.mp3"
+  },
+  {
+    "revision": "5145f38dd8088856e6e8cf31cee77d29",
+    "url": "/media/scene_13.5145f38d.mp3"
+  },
+  {
+    "revision": "80c697408a02d64eb228cd9a64eef083",
+    "url": "/media/scene_14.80c69740.mp3"
+  },
+  {
+    "revision": "0ccdbf4974d675e8417a8d264e3e8613",
+    "url": "/media/scene_15.0ccdbf49.mp3"
+  },
+  {
+    "revision": "55e694114795c6f6241588ae0a39cd1d",
+    "url": "/media/scene_16.55e69411.mp3"
+  },
+  {
+    "revision": "027529ba65b49c14503edf9eae0664f0",
+    "url": "/media/scene_17.027529ba.mp3"
+  },
+  {
+    "revision": "c8ad94dc99ada9eff4bb9269a440df3f",
+    "url": "/media/scene_18.c8ad94dc.mp3"
+  },
+  {
+    "revision": "af1bf5d39f031903ffa457480f818826",
+    "url": "/media/scene_2.af1bf5d3.mp3"
+  },
+  {
+    "revision": "e2bc374e2e1d423052bb63bea091131e",
+    "url": "/media/scene_3.e2bc374e.mp3"
+  },
+  {
+    "revision": "84f4e30e80e53196c95c456d70e4dd6d",
+    "url": "/media/scene_4.84f4e30e.mp3"
+  },
+  {
+    "revision": "28013de7ffb6235615767a6a32c6405d",
+    "url": "/media/scene_5.28013de7.mp3"
+  },
+  {
+    "revision": "200bd20aac33da9304ecb6dc8b4af3fc",
+    "url": "/media/scene_6.200bd20a.mp3"
+  },
+  {
+    "revision": "718d5edd789cedb676d5784d57fd2cc3",
+    "url": "/media/scene_7.718d5edd.mp3"
+  },
+  {
+    "revision": "54f7d246667ca891d63087569f34d726",
+    "url": "/media/scene_8.54f7d246.mp3"
+  },
+  {
+    "revision": "4ca2db931aa3cd083e5c74113f1c3855",
+    "url": "/media/scene_9.4ca2db93.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
